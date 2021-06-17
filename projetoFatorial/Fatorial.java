7419cf501e1293c7241b69871a118181c939a06e
Geraldojr12 <72410636+Geraldojr12@users.noreply.github.com>
2021-06-17 10:50:21 -0300

    Projeto Fatorial - Curso DevMedia

---
+++ projetoFatorial/Fatorial.java
@@ -0,0 +1,13 @@
+ public class Fatorial {
+	// versao iterativa do calculo do fatorial, incluindo o teste para n=0
+	public int calcularFatorial(int n) throws IllegalArgumentException {
+	    if (n < 0)
+		  throw new  IllegalArgumentException("não existe fatorial para numeros negativos");	
+	   int resultado = 1;
+	   if (n !=0) {		
+	   for (int i = 2; i <=n; i++)
+		resultado*= i;
+	   } 
+	   return resultado;
+	}
+}
---
+++ projetoFatorial/TesteFatorial.java
@@ -0,0 +1,9 @@
+class TesteFatorial {
+	// comentario inserido em TesteFatorial.java
+	public static void main(String [] args) {
+		Fatorial fat = new Fatorial();
+		int resultado = fat.calcularFatorial(4);
+		System.out.println(resultado);
+
+        }
+} 
